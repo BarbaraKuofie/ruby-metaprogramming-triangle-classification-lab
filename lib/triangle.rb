693c2f7010f826b4fbd36be287f1c8f459858d18
BarbaraKuofie <barbara.kuofie@gmail.com>
2020-09-26 23:36:09 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
-end
+  attr_reader :left, :right, :bottom
+
+  def initialize(left, right, bottom)
+    @left = left
+    @right  = right
+    @bottom = bottom
+  end
+   
+  def kind
+    if left == right && right == bottom && left == bottom
+     :equilateral 
+      elsif left == right || right == bottom || left == bottom
+        :isosceles 
+      elsif left != right && right != bottom && left != bottom 
+        :scalene 
+      else 
+        raise TriangleError
+    end 
+  end   
+  class TriangleError < StandardError
+        # illegal = 
+        # unequal = ((left + right) < bottom) || ((left + bottom) < right) || ((right + bottom) < left)
+        # if illegal || unequal
+        # end 
+  end
+end 
